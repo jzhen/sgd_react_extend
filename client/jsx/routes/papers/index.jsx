77f86aed9446de4deb0bc920b8a9e3b87a6d8444
Jimmy Zhen <jzhen@highwire.org>
2015-11-09 00:22:06 -0800

    Extending biolderplate functionality

--- client/jsx/routes/papers/index.jsx
+++ client/jsx/routes/papers/index.jsx
@@ -2,43 +2,68 @@
 var React = require("react");
 var Router = require("react-router");
 var { Route, DefaultRoute, RouteHandler, Link } = Router;
+var papers = require("./papers.js");
+
 
 var Index = React.createClass({
   getInitialState() {
     return {
-      data: [
-        {
-          id: "S000046605",
-          title: "Polarization of cell growth in yeast. I. Establishment and maintenance of polarity states. J Cell Sci 113 ( Pt 3):365-75"
-        },
-        {
-          id: "S000073427",
-          title: "Molecular cloning of the actin gene from yeast Saccharomyces cerevisiae"
-        }
-      ] 
+      data: papers,
+      nameFilter: ''
     };
   },
 
+  handleFilterUpdate: function(filterValue) {
+    this.setState({
+      nameFilter: filterValue
+    });
+  },
+
   render () {
+    var displayedItems = this.state.data.filter(function(item) {
+      var match = item.title.toLowerCase().indexOf(this.state.nameFilter.toLowerCase());
+      return (match !== -1);
+    }.bind(this));
+
     return (
       <div>
         <h1>Papers</h1>
-        {this.renderPaperList()}
+        <Filters updateFilter={this.handleFilterUpdate} />
+        <PaperList items={displayedItems} />
       </div>
     );
-  },
+  }
 
-  renderPaperList () {
-  	var listNodes = this.state.data
-  		.map( (d, i) => {
-  			return <div key={"paperList" + d.id}>
-    			<Link to="paper" params={d}>{d.title}</Link>
+});
+
+var PaperList = React.createClass({
+  render: function() {
+    var listNodes = this.props.items
+      .map( (d, i) => {
+        return <div key={"paperList" + d.id}>
+          <Link to="paper" params={d}>{d.title}</Link>
           <hr />
-  			</div>;
-  		});
-  	return (<div>
-  		{listNodes}
-	</div>);
+        </div>;
+      });
+    return (
+      <div>
+        {listNodes}
+      </div>);
+  }
+});
+
+var Filters = React.createClass({
+  handleFilterChange: function() {
+    var value = this.refs.filterInput.getDOMNode().value;
+    this.props.updateFilter(value);
+  },
+  render: function() {
+    return (
+      <div>
+        <input type="text" className="form-control search-filter" ref="filterInput" onChange={this.handleFilterChange} placeholder="Search papers" />
+        <br />
+      </div>
+    )
   }
 });
 
---
+++ client/jsx/routes/papers/papers.js
@@ -0,0 +1,15 @@
+/*
+    A list of paperss that can be required elsewhere in the application, like this:
+    var papers= require("papers");
+*/
+
+module.exports = [
+    {
+        id: "S000046605",
+        title: "Polarization of cell growth in yeast. I. Establishment and maintenance of polarity states. J Cell Sci 113 ( Pt 3):365-75"
+    },
+    {
+        id: "S000073427",
+        title: "Molecular cloning of the actin gene from yeast Saccharomyces cerevisiae"
+    }
+];
